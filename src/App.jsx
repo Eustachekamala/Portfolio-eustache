3a351c05086cf6aace1412bec0b5f8f963ff7a47
eustachekamala <eustachejared25@gmail.com>
2024-09-29 23:36:58 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+       <h1 className="text-4xl">Eustache Katembo</h1>
+       <p className="text-2xl">I am a web developer</p>
+       <p className="text-2xl">I love to build websites</p>
+       <div className="flex justify-center">
+         <a href="https://eustache.github.io/">Portfolio</a>
+         <a href="https://github.com/eustache">Github</a>
+         <a href="https://www.linkedin.com/in/eustache/">Linkedin</a>
+         <a href="https://twitter.com/eustache">Twitter</a>
+         <a href="https://www.instagram.com/eustache/">Instagram</a>
+       </div>
+    </div>
+  );
+}
+
+export default App;
