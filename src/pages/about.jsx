07c097345a7b0b44ff3c448d9f90dfa4188a3abe
eustachekamala <eustachejared25@gmail.com>
2024-09-30 18:30:53 +0300

    added the url for the resume

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,10 +1,22 @@
-
-
 function About() {
   return (
-    <div className="h-screen justify-center bg-gray-950">
-      <img className="h-52 w-52 border border-red-400" src="https://images.unsplash.com/photo-1678489828693-7d35c725ea69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" />
-      <p>I am a web developer</p>
+    <div className="h-screen flex flex-col items-center justify-center text-white bg-gray-950">
+      <div className="h-44 w-44">
+        <img
+          className="border-2 border-pink-500 rounded-full h-full w-full object-cover"
+          src="./src/assets/pexels-moh-adbelghaffar-771742.jpg"
+          alt="portfolio_image"
+        />
+      </div>
+      <p className="text-3xl py-4">I am a web developer</p>
+      
+      <a 
+        href="./src/assets/Resume Eustache.docx"
+        download
+        className="mt-6 bg-pink-500 hover:bg-pink-400 text-white font-semibold py-2 px-4 rounded transition duration-300"
+      >
+        Download Resume
+      </a>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      FontFamily: {
+        spaceGrotesk: ['Space Grotesk', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
