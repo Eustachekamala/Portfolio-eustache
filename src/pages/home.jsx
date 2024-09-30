08fbc2f97b825cd77e296cc91231b22c69758a71
eustachekamala <eustachejared25@gmail.com>
2024-09-30 18:53:06 +0300

    responsive web

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { useRef, useLayoutEffect } from 'react';
 
 function Home() {
-    const componentRef = useRef(null);
+  const componentRef = useRef(null);
 
   useLayoutEffect(() => {
     const csx = gsap.context(() => {
@@ -13,28 +13,30 @@ function Home() {
         y: 20,
         duration: 1,
       });
-
     }, componentRef);
 
     return () => csx.revert();
   }, []);
 
-    return (
-    <div className='relative' ref={componentRef}>
-        <div className='h-screen flex flex-col justify-center items-center bg-gray-950'>
-          <h1 id='welcome' className='text-5xl sm:text-7xl lg:text-9xl font-bold text-gray-100 font-spaceGrotesk'>
-            Welcome. I am <span className='text-pink-500'>Eustache</span>
-          </h1>
-          <button className='mt-10 bg-pink-500 text-white px-8 py-2 rounded-lg'>
-            <Link to='/About'>
-              <span className='text-xl sm:text-2xl lg:text-3xl font-bold'>
-                About Me
-              </span>
-            </Link>
-          </button>
-        </div>
+  return (
+    <div className="relative" ref={componentRef}>
+      <div className="h-screen flex flex-col justify-center items-center bg-gray-950 text-center">
+        <h1 
+          id="welcome" 
+          className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-bold text-gray-100 font-spaceGrotesk"
+        >
+          Welcome. I am <span className="text-pink-500">Eustache</span>
+        </h1>
+        <button className="mt-10 bg-pink-500 text-white px-6 py-2 rounded-lg transition duration-300 hover:bg-pink-400">
+          <Link to="/About">
+            <span className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold">
+              About Me
+            </span>
+          </Link>
+        </button>
       </div>
-      )
+    </div>
+  );
 }
 
 export default Home;
