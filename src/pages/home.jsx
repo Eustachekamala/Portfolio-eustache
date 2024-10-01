570aa189b77219b48925d18974433d2ca84abfb0
eustachekamala <eustachejared25@gmail.com>
2024-10-01 15:24:33 +0300

    update component home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -26,6 +26,10 @@ function Home() {
           className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-bold text-gray-100 font-spaceGrotesk"
         >
           Welcome. I am <span className="text-pink-500">Eustache</span>
+          <p className='text-xl p-8'>I'm a Tunisian based web designer & front‑end developer focused 
+            on crafting clean & user‑friendly experiences, 
+            I am passionate about building excellent software that 
+            improves the lives of those around me.</p>
         </h1>
         <button className="mt-10 bg-pink-500 text-white px-6 py-2 rounded-lg transition duration-300 hover:bg-pink-400">
           <Link to="/About">
