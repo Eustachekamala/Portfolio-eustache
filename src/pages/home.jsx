8b3ffa872c5f86c3db2cd254eaecbb8f7d81ce92
eustachekamala <eustachejared25@gmail.com>
2024-10-29 12:39:23 +0300

    update the home page of the  portfolio

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -27,14 +27,23 @@ function Home() {
       <Navbar />
       <div className='flex flex-col md:flex-row w-full pt-20 p-4'>
         <div className='flex flex-col py-8 justify-center items-center bg-gray-950 text-center pt-36 w-full md:w-2/5 border border-red-500'>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut quidem laborum magni possimus non quibusdam consequatur ullam commodi laudantium. Distinctio quod quidem libero hic exercitationem sed dolores, laborum fuga reiciendis?
-          Laudantium odio quae soluta earum reiciendis natus, tenetur ratione tempora minus minima perspiciatis corrupti dignissimos deleniti nesciunt cum est eligendi! Neque dolor praesentium rerum sed corrupti magni expedita reiciendis minus!
-          Eaque excepturi esse necessitatibus nemo adipisci, quis qui explicabo perferendis. Accusamus nesciunt eaque aspernatur nihil ratione rem distinctio iusto aliquid sit molestias quas sequi, voluptas reiciendis omnis ducimus repellendus dolor.
-          Quaerat reiciendis fugiat sed nesciunt aut dolores consectetur. Ab aut similique veritatis voluptas vel commodi non tempora dolores! Tempore dolorem fugiat quo unde aut obcaecati aliquid itaque, velit dignissimos. Magnam?</p>
+          <img src="michel.jpg" alt="michel" className="w-32 h-full rounded-full overflow-hidden shadow-lg" />
+          <p className="text-3xl text-white font-semibold py-4">Hi, I&apos;m Eustache</p>
+          <p className='text-lg sm:text-xl md:text-2xl p-4 mb-4 text-gray-100 font-spaceGrotesk'>
+            I&apos;m a Full-Stack Engineer with a passion for building scalable and user-friendly applications. 
+            I&apos;m excited to bring my expertise to the tech industry and contribute to the growth of the industry.
+          </p>
+          <a 
+            href="./src/assets/Resume Michel.docx"
+            download
+            className="bg-pink-500 hover:bg-pink-400 text-white font-semibold py-2 px-4 rounded transition duration-300 shadow-md hover:shadow-lg"
+          >
+            Download Resume
+          </a>
         </div>
         
         <div id="welcome" className="flex flex-col py-8 justify-center items-center bg-gray-950 text-center pt-36 w-full md:w-3/5 border border-blue-500">
-          <div className="h-44 w-44 mb-6 rounded-full overflow-hidden shadow-lg">
+          {/* <div className="h-44 w-44 mb-6 rounded-full overflow-hidden shadow-lg">
             <img
               className="h-full rounded-full w-full object-cover border-4 border-pink-500"
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmh903RuvBCgjlWDeCFJehx7Xu6FJNu4ADCw&s"
@@ -55,7 +64,7 @@ function Home() {
             className="bg-pink-500 hover:bg-pink-400 text-white font-semibold py-2 px-4 rounded transition duration-300 shadow-md hover:shadow-lg"
           >
             Download Resume
-          </a>
+          </a> */}
         </div>
       </div>
       <About />
