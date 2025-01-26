76b6c7bc65a3184c9a7b67ef769e99a262d7b887
eustachekamala <eustachejared25@gmail.com>
2025-01-26 23:05:19 +0300

    changed the project component

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -29,18 +29,6 @@ function Projects() {
                         their meals.
                     </p>
                 </div>
-                <div className="bg-gray-800 rounded-lg p-4 flex flex-col justify-center items-center">
-                    <h2 className="text-xl sm:text-sm md:text-base lg:text-lg xl:text-xl font-bold mb-4">FreshGo App</h2>
-                    <img className="w-full h-auto rounded-lg mb-2" src="FreshGo.png" alt="" />
-                    <p className="text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl mt-2 text-center">FreshGo is a modern grocery shopping website 
-                        designed to enhance your shopping 
-                        experience. With features like recipe 
-                        generation, a shopping cart, and item filtering, 
-                        FreshGo makes grocery shopping efficient and enjoyable. 
-                        Additionally, users can discover drink recipes to complement 
-                        their meals.
-                    </p>
-                </div>
             </div>
         </div>
     );
