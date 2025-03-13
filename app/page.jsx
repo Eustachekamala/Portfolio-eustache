b701b8981af3b579bc687dfde93066720a4d05c2
eustachekamala <eustachejared25@gmail.com>
2025-03-13 23:19:17 +0300

    Added the shadcn components

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-blue-400 text-5xl">Home page</h1>
+      <button className="btn-primary">Click</button>
+      <div className="animate-appear">Appearing content</div>
+      <div className="animate-slide">Sliding content</div>
+    </div>
+  );
+}
